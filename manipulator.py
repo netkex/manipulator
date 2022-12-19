cfdbcd25fa5a75354de676d4c3a08795db9e97b5
Daniil Pavlenko <dp.3481088@gmail.com>
2022-12-20 00:38:19 +0300

    init commit

---
+++ manipulator.py
@@ -0,0 +1,69 @@
+from typing import Optional, List
+import numpy as np
+
+
+class Manipulator(): 
+    def __init__(self, 
+        joint_num: int, 
+        angle_delta: Optional[float] = np.pi / 180,
+        joint_angles: Optional[List[float]] = None, 
+        arm_len: Optional[List[float]] = None):
+        assert (joint_num >= 3) 
+
+        self.joint_num = joint_num
+        self.angle_delta = angle_delta
+
+        if joint_angles is None: 
+            self.joint_angles = [0] + [np.pi] * (joint_num - 1)
+        else: 
+            self.joint_angles = joint_angles
+
+        if arm_len is None: 
+            self.arm_len = [1] * (joint_num - 1)
+        else: 
+            self.arm_len = arm_len
+        
+        self.joint_coordinates = None  # lazy init
+        
+    def get_joint_coordinates(self) -> np.ndarray: 
+        if self.joint_coordinates is not None: 
+            return self.joint_coordinates
+        self.joint_coordinates = np.zeros((self.joint_num, 3))
+        self.joint_coordinates[1] = np.array([
+            self.arm_len[0] * np.cos(self.joint_angles[1]), 
+            0, 
+            self.arm_len[0] * np.sin(self.joint_angles[1])
+        ])
+        for i in range(1, self.joint_num - 1): 
+            prev_arm_vec = self.joint_coordinates[i] - self.joint_coordinates[i - 1]
+            new_arm_vec = prev_arm_vec / np.linalg.norm(prev_arm_vec) * self.arm_len[i]
+            new_arm_vec[[0, 2]] = Manipulator.rotation_matrix_2d(np.pi + self.joint_angles[i + 1]) @ new_arm_vec[[0, 2]] 
+            self.joint_coordinates[i + 1] = self.joint_coordinates[i] + new_arm_vec
+        turn_around_rot = Manipulator.rotation_matrix_2d(self.joint_angles[0])
+        for i in range(0, self.joint_num):
+            self.joint_coordinates[i][[0, 1]] = turn_around_rot @ self.joint_coordinates[i][[0, 1]]
+        return self.joint_coordinates
+
+    def get_successors(self): 
+        successors = []
+        for i in range(self.joint_num): 
+            for dangle in [-self.angle_delta, self.angle_delta]:
+                new_joint_angle = self.joint_angles[i] + dangle
+                if new_joint_angle < 2 * -np.pi: 
+                    new_joint_angle += 2 * np.pi 
+                if new_joint_angle > 2 * np.pi: 
+                    new_joint_angle -= 2 * np.pi 
+                successors.append(Manipulator(
+                    self.joint_num,
+                    self.angle_delta, 
+                    self.joint_angles[:i] + [new_joint_angle] + self.joint_angles[i+1:],
+                    self.arm_len 
+                ))
+        return successors
+
+    @staticmethod
+    def rotation_matrix_2d(angle: float) -> np.ndarray: 
+        return np.array([
+            [np.cos(angle), -np.sin(angle)],
+            [np.sin(angle), np.cos(angle)]
+        ])
---
+++ obstacle.py
@@ -0,0 +1,25 @@
+from manipulator import Manipulator
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Obstacle(ABC):
+    @abstractmethod
+    def intersect(self, manipulator: Manipulator) -> bool:
+        pass
+
+
+class SphereObstacle(Obstacle): 
+    def __init__(self, center: np.ndarray, r: float): 
+        self.center = center
+        self.r = r 
+
+    def intersect(self, manipulator: Manipulator) -> bool:
+        joint_coordinates = manipulator.get_joint_coordinates() 
+        for i in range(len(joint_coordinates) - 1): 
+            if np.linalg.norm(joint_coordinates[i] - self.center) < self.r or np.linalg.norm(joint_coordinates[i + 1] - self.center) < self.r: 
+                return True
+            arm_vec = joint_coordinates[i + 1] - joint_coordinates[i] 
+            t = (self.center @ arm_vec - joint_coordinates[i] @ arm_vec) / (np.linalg.norm(arm_vec) ** 2)  # arm contains all points: joint_coordinates[i] + t * arm_vec, if t \in [0, 1]
+            if 0 <= t <= 1: 
+                return True
+        return False
